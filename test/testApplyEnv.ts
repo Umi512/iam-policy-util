1face0002d16d764c89973af584d373c536870be
ManabeKen <ken512.k.m.512@yahoo.ne.jp>
2023-01-03 02:44:53 +0900

    テストデータ見直し

--- test/testApplyEnv.ts
+++ test/testApplyEnv.ts
@@ -1,7 +1,18 @@
 import { setJsonKey } from "/lib/applyEnv.ts";
+import { assertObjectMatch } from "testing/asserts.ts";
 
-// Simple name and function, compact form, but not configurable
-Deno.test("./lib/applyEnv.ts", () => {
+Deno.test("json object check", async() => {
   const filepath = "./sample.json";
-  setJsonKey(filepath);
+  const jsonData: Record<string,unknown> = await setJsonKey(filepath);
+  console.log(jsonData);
+  assertObjectMatch(jsonData, {
+    hoge: "1",
+    hoge2: "foo",
+    hoge3: "[1,2,3]",
+    hoge4: "{a:1,b:foo}",
+    hoge5: "[1,2,{a:1,b:foo,c:[1,2,3]}]",
+    hoge6: "{a:1,b:2,c:[1,2,{a:1,b:foo}]}",
+    hoge7: "true",
+    hoge8: ""
+  });
 });
