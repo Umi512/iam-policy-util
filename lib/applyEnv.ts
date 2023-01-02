3dd0efebadf99215691392c9fb91736a1065158b
ManabeKen <ken512.k.m.512@yahoo.ne.jp>
2023-01-03 00:23:23 +0900

    node test実施 applyEnv.ts

--- lib/applyEnv.ts
+++ lib/applyEnv.ts
@@ -2,18 +2,18 @@
 概要：// .envファイルから環境設定値を取得し，jsonファイルのプレースホルダーを置き換える
 */
 
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { config, DotenvConfig } from "dotenv/mod.ts";
 
-export async function setJsonKey(filepath:string){
+export async function setJsonKey(filepath: string): Promise<void> {
     try{
-        let checkFlag:boolean = true;
-        const env = config({ safe: true });
-        const jsonData = await getJsonObj(filepath);
-        if(!jsonData){
+        let checkFlag: boolean = true;
+        const env: DotenvConfig = config({ safe: true });
+        const jsonData: Record<string, unknown> = await getJsonObj(filepath);
+        if(Object.keys(jsonData).length <= 0) {
             throw new Error();
         }
         checkFlag = replaceJsonPlaceHolder(jsonData,env);
-        if(!checkFlag){
+        if(!checkFlag) {
             throw new Error();
         }
         console.log(jsonData);
@@ -23,21 +23,21 @@ export async function setJsonKey(filepath:string){
 }
 
 // jsonファイルからjsonオブジェクトを取得する関数
-async function getJsonObj(filePath: string) :Object{
+async function getJsonObj(filePath: string): Promise<Record<string, unknown>> {
     try {
         return JSON.parse(await Deno.readTextFile(filePath));
     } catch(e) {
         console.log(filePath + " : " + e.message);
-        return undefined;
+        return {};
     }
 }
 
 // jsonファイルの${}を，.env環境変数ファイルの各設定値に置き換える
-function replaceJsonPlaceHolder(jData: Object,env: Object): boolean{
+function replaceJsonPlaceHolder(jData: Record<string, unknown>,env: Record<string, string>): boolean{
     try{
-        for(let eKey in env){
-            for(let jKey in jData){
-                if("${" + eKey + "}" == jData[jKey]){
+        for(const eKey in env) {
+            for(const jKey in jData) {
+                if("${" + eKey + "}" == jData[jKey]) {
                     jData[jKey] = env[eKey];
                 }
             }
@@ -48,3 +48,24 @@ function replaceJsonPlaceHolder(jData: Object,env: Object): boolean{
         return false;
     }
 }
+/*function replaceJsonPlaceHolder(jData: Record<string, unknown>,env: Record<string, string>): boolean{
+    try{
+        for(const eKey in env) {
+            for(const jKey in jData) {
+                if("${" + eKey + "}" == jData[jKey]) {
+                    if(!isNaN(env[eKey])) {
+                        jData[jKey] = Number(env[eKey]);
+                    } else if() {
+
+                    } else {
+                        jData[jKey] = env[eKey];
+                    }
+                }
+            }
+        }
+        return true;
+    } catch(e) {
+        console.log(e.message);
+        return false;
+    }
+}*/
---
+++ test/testApplyEnv.ts
@@ -0,0 +1,7 @@
+import { setJsonKey } from "/lib/applyEnv.ts";
+
+// Simple name and function, compact form, but not configurable
+Deno.test("./lib/applyEnv.ts", () => {
+    const filepath = "./sample.json";
+    setJsonKey(filepath);
+});
