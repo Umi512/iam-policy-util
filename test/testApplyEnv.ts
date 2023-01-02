74ea1ed222974b055978d127c95ff6f0db0ee148
ManabeKen <ken512.k.m.512@yahoo.ne.jp>
2023-01-03 01:27:42 +0900

    不要なimportを削除

--- test/testApplyEnv.ts
+++ test/testApplyEnv.ts
@@ -1,9 +1,7 @@
 import { setJsonKey } from "/lib/applyEnv.ts";
-import { assertArrayContains, assertEquals } from "testing/asserts.ts";
 
 // Simple name and function, compact form, but not configurable
 Deno.test("./lib/applyEnv.ts", () => {
   const filepath = "./sample.json";
   setJsonKey(filepath);
-  assertArrayContains([1, 2, 3, 4, 5, 6], [3], "Expected 3 to be in the array");
 });
